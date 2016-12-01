b469de79347a75b518c01b357f9944d5dcab4620
jose-fernandez <jose.fernandez91@gmail.com>
2016-12-01 12:19:13 +0100

    in process

--- arcanoid.js
+++ arcanoid.js
@@ -19,8 +19,9 @@ class game{
 	}
 	createElements(){
 		this.ball=new ball(200,450,2,1,10);
-		//this.bar=new block(160,462,80,10); Traditional way
-		this.bar=new bar();
+		this.bar=new block(160,460,80,10);
+		this.bar.eventBar();
+		//this.bar=new bar();
 
 		for (let i=10;i<106;i+=19){
 			for(let j=10;j<335;j+=54){
@@ -52,8 +53,6 @@ for (let grad=0; grad<360; grad+=45) {
 		cir.setAttribute("cx",`${this.cx}`);
 		cir.setAttribute("cy",`${this.cy}`);
 		cir.setAttribute("r",`${this.r}`);
-		cir.setAttribute("stroke", `black`);
-		cir.setAttribute("stroke-width", 2);
 		cir.setAttribute("fill", "white");
 
 		document.getElementById("container").appendChild(cir);
@@ -105,37 +104,75 @@ class block{
 		this.w=w;
 		this.h=h;
 		this.build();
+		
 	}
 	build(){
-		var rec= document.createElementNS ("http://www.w3.org/2000/svg", "rect");
-		rec.setAttribute("x",`${this.x}`);
-		rec.setAttribute("y",`${this.y}`);
-		rec.setAttribute("width",`${this.w}`);
-		rec.setAttribute("height",`${this.h}`);
-		rec.setAttribute("stroke", `black`);
-		rec.setAttribute("stroke-width", 2);
-		rec.setAttribute("fill", "white");
+		this.rec= document.createElementNS ("http://www.w3.org/2000/svg", "rect");
+		this.rec.setAttribute("x",`${this.x}`);
+		this.rec.setAttribute("y",`${this.y}`);
+		this.rec.setAttribute("width",`${this.w}`);
+		this.rec.setAttribute("height",`${this.h}`);
+		this.rec.setAttribute("fill", "white");
 
-		document.getElementById("container").appendChild(rec);
+		document.getElementById("container").appendChild(this.rec);
+	}
+	eventBar(){
+	var that=this;
+	window.addEventListener("keydown",function(e){
+		if(e.key=="ArrowRight")
+			that.move(10,that);
+		else if(e.key=="ArrowLeft")
+			that.move(-10,that);
+	});
+	}
+	move(e,x){
+		if(this.x>0 && this.x<320){
+			this.x+=e;
+			this.rec.setAttribute("x",`${this.x}`);
+		}else{
+			if(this.rec.x.baseVal.value==0 && e.key=="ArrowRight"){
+				this.x+=e;
+				this.rec.setAttribute("x",`${this.x}`);
+			}else if(this.x==320 && e.key=="ArrowLeft"){
+				this.x+=e;
+				this.rec.setAttribute("x",`${this.x}`);
+			}
+			
+		}
+
 	}
 }
 
-class bar{
+/*class bar{
 	constructor(){
 		this.build();
+		this.event();
 	}
 	build(){
-		var pol= document.createElementNS ("http://www.w3.org/2000/svg", "polyline");
-		pol.setAttribute("points",`150, 440, 200, 480, 250, 440`);//points=x,y;x,y;x,y
-		pol.setAttribute("stroke", `black`);
-		pol.setAttribute("stroke-width", 15);
-		pol.setAttribute("fill", "none");
-		pol.setAttribute("stroke-linejoin", "bevel");
-		pol.setAttribute("stroke-linecap", "bevel");
+		this.pol= document.createElementNS ("http://www.w3.org/2000/svg", "polyline");
+		this.pol.setAttribute("points",`150, 440, 200, 480, 250, 440`);//points=x,y;x,y;x,y
+		this.pol.setAttribute("stroke", `black`);
+		this.pol.setAttribute("stroke-width", 15);
+		this.pol.setAttribute("fill", "none");
+		this.pol.setAttribute("stroke-linejoin", "bevel");
+		this.pol.setAttribute("stroke-linecap", "bevel");
 
-		document.getElementById("container").appendChild(pol);
+		document.getElementById("container").appendChild(this.pol);
 	}
-}
+	event(){
+		var that=this;
+		window.addEventListener("keydown",function(e){
+			if(e.key=="ArrowRight")
+				that.move(1,that);
+			else if(e.key=="ArrowLeft")
+				that.move(-1,that);
+		});
+	}
+	move(e,x){
+		console.log(x.pol.baseVal.value);
+		x.pol.setAttribute("points",`150+${e}, 440, 200+${e}, 480, 250+${e}, 440`);
+	}}*/
+
 
 class controller{
 	constructor(){
