e56546fc56ebb1c9e26f9990e99ccdda5e966b67
jose-fernandez <jose.fernandez91@gmail.com>
2016-12-03 13:22:07 +0100

    paused

--- arcanoid.js
+++ arcanoid.js
@@ -7,6 +7,7 @@ class game{
 		this.list=[];
 		this.build();
 		this.createElements();
+		this.run();
 	}
 	build(){
 		var bg= document.createElementNS ("http://www.w3.org/2000/svg", "svg");
@@ -19,8 +20,10 @@ class game{
 	}
 	createElements(){
 		this.ball=new ball(200,450,2,1,10);
+		this.list.push(this.ball);
 		this.bar=new block(160,460,80,10);
-		this.bar.eventBar();
+		this.list.push(this.bar);
+		this.list[1].eventBar();
 		//this.bar=new bar();
 
 		for (let i=10;i<106;i+=19){
@@ -29,7 +32,12 @@ class game{
 			}
 		}		
 	}
-
+	run(){
+		var that=this;
+		setInterval(function(){
+			that.list[0].calculateMove(that, that.list);
+		}, 10);
+	}
 }
 
 class ball{
@@ -39,11 +47,6 @@ class ball{
 		this.r=r;
 		this.vx=vx;
 		this.vy=vy;
-/*Cálculo de 8 puntos
-for (let grad=0; grad<360; grad+=45) {
-	var p_x = self.bolas[i].r * Math.cos(grad) + self.bolas[i].x;
-	var p_y = self.bolas[i].r * Math.cos(grad) + self.bolas[i].y;
-}*/
 
 		this.build();
 	}
@@ -58,26 +61,28 @@ for (let grad=0; grad<360; grad+=45) {
 		document.getElementById("container").appendChild(cir);
 	}
 
-	calculateMove(x){
-		var eli=document.getElementsByTagName("ellipse");
-		if(this.py>(x.y-(this.ry+this.st)) && this.px<(0+this.rx+this.st) ||
-			this.py>(x.y-(this.ry+this.st)) && this.px>(x.x-(this.rx+this.st))|| 
-			this.py<(0+this.ry+this.st) && this.px>(x.x-(this.rx+this.st)) ||
-			this.py<(0+this.ry+this.st) && this.px<(0+this.rx+this.st)){
-				eli[this.id].setAttribute("rx",`${this.rx}`);
-				this.rx=10;
-				this.vx=this.vx*(-1);
-				eli[this.id].setAttribute("ry",`${this.ry}`);
-				this.ry=10;
-				this.vy=this.vy*(-1);
+	calculateMove(limit, list){
+		var cir=document.getElementsByTagName("circle");
+		this.collisionLimit(limit, list);
 
-		}else if (this.py>(x.y-(this.ry+this.st)) || this.py<(0+this.ry+this.st)){
+		this.cx+=this.vx;
+		this.cy+=this.vy;
+
+		cir[0].setAttribute("cx",this.cx);
+		cir[0].setAttribute("cy",this.cy);
+
+
+/*
+		if(this.cy>x.y && this.cx<0){
+			this.ry=10;
+			this.vy=this.vy*(-1);
+		}else if (this.cy>(x.y-(this.ry+this.st)) || this.cy<(0+this.ry+this.st)){
 			this.vy=this.vy*(-1);
 			eli[this.id].setAttribute("rx",13);
 			this.rx=13;
 			eli[this.id].setAttribute("ry",7);
 			this.ry=7;
-		}else if(this.px>(x.x-(this.rx+this.st)) || this.px<(0+this.rx+this.st)){
+		}else if(this.cx>(x.x-(this.rx+this.st)) || this.cx<(0+this.rx+this.st)){
 			this.vx=this.vx*(-1);
 			eli[this.id].setAttribute("ry",13);
 			this.ry=13;
@@ -89,11 +94,35 @@ for (let grad=0; grad<360; grad+=45) {
 			eli[this.id].setAttribute("ry",`${this.ry}`);
 			this.ry=10;
 		}
-		this.px=this.px+this.vx;
-		eli[this.id].setAttribute("cx",`${this.px}`);
+		this.cx=this.cx+this.vx;
+		eli[this.id].setAttribute("cx",`${this.cx}`);
 
-		this.py=this.py+this.vy;
-		eli[this.id].setAttribute("cy",`${this.py}`);
+		this.cy=this.cy+this.vy;
+		eli[this.id].setAttribute("cy",`${this.cy}`);*/
+	}
+	collisionLimit(limit, list){
+		if(this.cy>=(list[1].y - this.r)){
+			this.collisionBar(list[1]);
+		}
+		if(this.cx>=(limit.x-this.r) || this.cx<=(0+this.r)){
+			/*for (let grad=0; grad<360; grad+=45) {
+				var cx = this.cx * Math.cos(grad) + self.bolas[i].x;
+				var cy = self.bolas[i].r * Math.cos(grad) + self.bolas[i].y;
+			}*/
+			this.vx=this.vx*(-1);
+		}else if(this.cy>=(limit.y-this.r) || this.cy<=(0+this.r)){
+			this.vy=this.vy*(-1);
+		}
+	}
+	collisionBar(bar){
+		if(this.cx>=bar.x && this.cx<=(bar.x+bar.w)){
+			if(this.cy>=(bar.y-this.r) && (this.cy<=(bar.y+bar.h+this.r))){
+				this.vx=this.vx*(-1);
+			}else{
+				this.vy=this.vy*(-1);
+			}
+			
+		}
 	}
 }
 
@@ -104,6 +133,7 @@ class block{
 		this.w=w;
 		this.h=h;
 		this.build();
+
 		
 	}
 	build(){
@@ -120,26 +150,22 @@ class block{
 	var that=this;
 	window.addEventListener("keydown",function(e){
 		if(e.key=="ArrowRight")
-			that.move(10,that);
+			that.moveBarRight(20,that);
 		else if(e.key=="ArrowLeft")
-			that.move(-10,that);
+			that.moveBarLeft(-20,that);
 	});
 	}
-	move(e,x){
-		if(this.x>0 && this.x<320){
+	moveBarLeft(e,x){
+		if(this.x>0){
 			this.x+=e;
 			this.rec.setAttribute("x",`${this.x}`);
-		}else{
-			if(this.rec.x.baseVal.value==0 && e.key=="ArrowRight"){
-				this.x+=e;
-				this.rec.setAttribute("x",`${this.x}`);
-			}else if(this.x==320 && e.key=="ArrowLeft"){
-				this.x+=e;
-				this.rec.setAttribute("x",`${this.x}`);
-			}
-			
+		}	
+	}
+	moveBarRight(e,x){
+		if(this.x<320){
+			this.x+=e;
+			this.rec.setAttribute("x",`${this.x}`);
 		}
-
 	}
 }
 
@@ -176,10 +202,9 @@ class block{
 
 class controller{
 	constructor(){
-		this.game = new game(400,500);
+		this.vG = new game(400,500);
 	}
 }
-
 
 window.onload=function(){
 	new controller();
